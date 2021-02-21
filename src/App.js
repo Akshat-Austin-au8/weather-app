b2005cf7af0f1d6c58bb7a65072cb69ff5bbcd4b
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-02-21 20:47:15 +0530

    Weathwe app start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Weather from './app_component/weather.component'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <Weather />
+    </div>
+  )
+}
+
+export default App
